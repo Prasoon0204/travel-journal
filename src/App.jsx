9d22d2c02070c82f7e18a016fa1a18d92d593fbb
Prasoon Thakur <prasoon.thakur0204@gmail.com>
2024-12-20 21:35:47 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import Card from './components/Card'
 import Header from './components/Header'
+import data from './data'
 function App() {
   return (
     <>
       <Header />
-      <Card />
+      {data.map((items) => (
+        <Card 
+        key={items.id}
+        {...items}
+        />
+      ))}
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,17 @@
-const Card = () => {
+/* eslint-disable react/prop-types */
+const Card = (props) => {
   return (
     <div className="flex font-inter rounded-md m-16 gap-5">
-        <img className="rounded-md" src="https://scrimba.com/links/travel-journal-japan-image-url" alt="card-image" width={140} height={168}/>
+        <img className="rounded-md" src={props.img.src} alt={props.img.alt} width={140} height={168}/>
         <div className="py-5">
             <div className="flex items-center mt-4">
                 <img className="mr-1" src="/src/assets/location.png" alt="location" width={12}/>
-                <p className="pr-4 text-xs text-blue-950">JAPAN</p>
-                <a className="text-xs text-gray-400 underline" href="https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9">View On Google Maps</a>
+                <p className="pr-4 text-xs text-blue-950">{props.country}</p>
+                <a className="text-xs text-gray-400 underline" href={props.googleMapsLink}>View On Google Maps</a>
             </div>
-            <div className="font-bold text-2xl my-2">Mount Fuji</div>
-            <p className="text-xs mt-4 mb-2 font-semibold">12 Jan, 2021 - 24 Jan, 2021</p>
-            <p className="text-xs text-blue-950">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+            <div className="font-bold text-2xl my-2">{props.title}</div>
+            <p className="text-xs mt-4 mb-2 font-semibold">{props.dates}</p>
+            <p className="text-xs text-blue-950">{props.text}</p>
         </div>
     </div>
   )
